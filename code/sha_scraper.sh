a7fa1b20cc8b1a1dbb9a1ff8d1d5eca94d659667
rmanaem <rmanaem@protonmail.ch>
2024-05-06 15:44:13 -0400

    Updated `sha_scraper` script
    - Added echo statements
    - Removed dead code

--- code/sha_scraper.sh
+++ code/sha_scraper.sh
@@ -12,49 +12,12 @@ OWNER="OpenNeuroDatasets-JSONLD"
 # }' | jq -r '.data.organization.repositories.totalCount')
 # Return every repository name except .github (because that one is special)
 nRepos=5
-reposON_LD=$(gh repo list "OpenNeuroDatasets-JSONLD" --limit ${nRepos} --json name --jq '.[].name' | grep -v ".github")
-
-# check if sha.txt doesn't exists
-#TODO refactor reusable part of the loop into a function
-# if [ ! -f sha.txt ]; then
-#     for repo in $reposON_LD; do
-
-#         # This block gets the default branch, but we might not need it because API call to fetch latest SHA *should* auto do so from default branch
-#         # response=$(gh api graphql -f query='query GetDefaultBranch($owner: String!, $name: String!) {
-#         #     repository(name: $name, owner: $owner) {
-#         #         defaultBranchRef {
-#         #             branchName: na me
-#         #         }
-#         #     }
-#         # }' -f owner="${OWNER}" -f name="${repo}")
-#         # defaultBranch=$(echo $response | jq -r '.data.repository.defaultBranchRef.branchName')
-
-#         # if it doesn't we need to create it and then run the following to populate for the first time
-        
-#         # need to test if the sha we grab here changes with a new commit
-#         sha=$(curl -L \
-#             -H "Accept: application/vnd.github+json" \
-#             -H "Authorization: Bearer ${GH_TOKEN}" \
-#             -H "X-GitHub-Api-Version: 2022-11-28" \
-#             https://api.github.com/repos/${OWNER}/${repo}/commits | jq .[0].sha)
-        
-#         echo $repo,$sha >> sha.txt
-
-#     done
+# We need to add 1 to num of repos b/c by default .github will always be first in the list
+reposON_LD=$(gh repo list "OpenNeuroDatasets-JSONLD" --limit $((nRepos+1)) --json name --jq '.[].name' | grep -v ".github")
 
 for repo in $reposON_LD; do
-# TODO Gets default branch, delete if not needed
-# response=$(gh api graphql -f query='query GetDefaultBranch($owner: String!, $name: String!) {
-#     repository(name: $name, owner: $owner) {
-#         defaultBranchRef {
-#             branchName: name
-#         }
-#     }
-# }' -f owner="${OWNER}" -f name="${repo}")
-# defaultBranch=$(echo $response | jq -r '.data.repository.defaultBranchRef.branchName')
-
-    # Get the SHA of the latest commit in the repo
-    # need to test if the sha we grab here changes with a new commit
+    # Get the SHA of the latest commit in the repo (NOTE: This will always be from the default branch)
+    # TODO: Test if the SHA we grab here changes with a new commit
     sha=$(curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GH_TOKEN}" \
@@ -66,11 +29,12 @@ for repo in $reposON_LD; do
 
     # If line with repo SHA is found
     if [ ! -z "$line" ]; then
-        echo "SHA found in file"
+        echo "${repo}: SHA found in file"
         old_sha=$(echo $line | cut -d, -f2)
-        # if the sha is not the same as the old one
+        # if the SHA is not the same as the old one
         if [ $old_sha != "$sha" ]; then
             # check if participants.tsv exists
+            echo "${repo}: latest SHA is different than existing SHA"
             participant_tsv_http_code=$(curl -s -o /dev/null -w "%{http_code}" \
             -H "Accept: application/vnd.github+json" \
             -H "Authorization: Bearer ${GH_TOKEN}" \
@@ -85,16 +49,17 @@ for repo in $reposON_LD; do
             https://api.github.com/repos/${OWNER}/${repo}/contents/participants.json)
 
             if (( ($participant_tsv_http_code > 200 || $participant_tsv_http_code < 300 ) && ($participant_json_http_code > 200 || $participant_json_http_code < 300 ))); then
+                echo "${repo}: running the CLI"
                 ./run-cli-single-dataset.sh $repo
             fi
 
-            #replace the old sha with the new one
+            # Replace the old SHA with the new one
+            echo "${repo}: Updating SHA in file"
             sed -i "s/${line}/${repo},${sha}/" sha.txt
         fi   
     # Otherwise, write current SHA to file
     else
+        echo "${repo}: SHA not found, writing latest SHA to file"
         echo $repo,$sha >> sha.txt
     fi
 done
-# fi
-
