da771e6488d4fb62b1237d40a996327eaf191278
Alyssa Dai <alyssa.ydai@gmail.com>
2024-05-10 02:41:27 -0400

    fix problems with run_cli_single_dataset.sh

--- code/run_cli_single_dataset.sh
+++ code/run_cli_single_dataset.sh
@@ -17,15 +17,13 @@ ds_git="git@github.com:OpenNeuroDatasets-JSONLD/${ds_id}"
 
 
 ldin=data
+mkdir -p ${ldin}
 ldout=data/jsonld
+mkdir -p ${ldout}
 
 # NOTE: realpath is needed to get an absolute path of the directory for mounting, 
 # otherwise Docker will error out at the mount step
 workdir=$(realpath ${ldin}/${ds_id})
-
-mkdir -p ${ldin}
-mkdir -p ${ldout}
-
 out="${ldout}/${ds_id}.jsonld"
 
 datalad clone ${ds_portal} ${workdir}
@@ -38,6 +36,6 @@ ds_name=$(python extract_bids_dataset_name.py --ds $workdir)
 if [ "$ds_name" == "None" ] ; then ds_name=$ds_id ; else ds_name=$ds_name ; fi
 
 # Run the Neurobagel CLI
-docker run --rm -v ${workdir}:${workdir} neurobagel/bagelcli pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name "$ds_name" --portal $ds_portal
-docker run --rm -v ${workdir}:${workdir} neurobagel/bagelcli bids --jsonld-path ${workdir}/pheno.jsonld  --bids-dir ${workdir} --output ${workdir}
+docker run --rm -v ${workdir}:${workdir} neurobagel/bagelcli pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir}/pheno.jsonld --name "$ds_name" --portal $ds_portal
+docker run --rm -v ${workdir}:${workdir} neurobagel/bagelcli bids --jsonld-path ${workdir}/pheno.jsonld  --bids-dir ${workdir} --output ${workdir}/pheno_bids.jsonld
 cp ${workdir}/pheno_bids.jsonld ${out}
