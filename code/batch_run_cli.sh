b4e2bed170dec3627961ee4d7429e20e14c8bb81
Alyssa Dai <alyssa.ydai@gmail.com>
2024-05-10 00:17:32 -0400

    refactor batch_run_cli.sh

--- code/batch_run_cli.sh
+++ code/batch_run_cli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Each repo ID to run the CLI on (and their corresponding SHA) is on a separate line
+# A space-separated list where each item is: <repo ID>,"<SHA>"
 dataset_list=$1
 
-for dataset in $(cat "$dataset_list"); do
+for dataset in $dataset_list; do
     repo=$(echo $dataset | cut -d ',' -f 1)
     sha=$(echo $dataset | cut -d ',' -f 2)
 
@@ -11,13 +11,12 @@ for dataset in $(cat "$dataset_list"); do
     ./run_cli_single_dataset.sh $repo
 
     # If the CLI did not run successfully, exit loop without updating SHA
-    if [ $? -ne 0 ]; then
-        echo "${repo}: CLI run failed"
-        break
+    if [ $? -eq 0 ]; then
+        # Replace the old SHA with the new one
+        echo "${repo}: Updating SHA in file"
+        line=$(grep "$repo" sha.txt)
+        sed -i "s/${line}/${repo},${sha}/" sha.txt
+    else
+        echo "${repo}: CLI failed"
     fi
-
-    # Replace the old SHA with the new one
-    echo "${repo}: Updating SHA in file"
-    line=$(grep "$repo" sha.txt)
-    sed -i "s/${line}/${repo},${sha}/" sha.txt
 done
