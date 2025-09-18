b8f59edd7f0b97609edbaa03349ccbe15e211b6e
Alyssa Dai <alyssa.ydai@gmail.com>
2025-09-18 13:23:58 -0400

    [ENH] Make CLI runner workflows smarter about which datasets to run the CLI on (#39)
    * use function exit codes and improve script hygiene
    
    * silence curl progress in sha_scraper.sh
    
    * rename job for clarity
    
    * for datasets missing annotations, record IDs and update sha.txt
    
    * restore non-zero exit code when bids command fails
    
    * add check that >=1 JSONLD was created and add wf flag for whether to remove old JSONLDs
    - also standardize wf input naming
    
    * store list of datasets missing annotations in file

--- code/batch_run_cli.sh
+++ code/batch_run_cli.sh
@@ -6,37 +6,55 @@
 # A space-separated list where each item is: <repo ID>,<SHA>
 dataset_list_path=$1
 
+update_sha_file() {
+    local repo=$1
+    local sha=$2
+    local old_repo_sha
+
+    # Look for an existing SHA for the repo in sha.txt
+    old_repo_sha=$(grep "$repo" sha.txt)
+
+    # The following section is similar to code in sha_scraper.sh, but instead updates SHAs in sha.txt for
+    # each repo that has *successfully run the CLI*, to ensure we now record their most up to date SHA
+    # (i.e., to indicate when the last successful CLI run happened).
+    # To achieve this:
+    # - if the repo DOES already have an entry in sha.txt, we replace (update) the existing SHA with the current SHA
+    # - if the repo DOES NOT already have an entry in sha.txt (meaning this is a newly added repo), add the repo and its SHA
+    if [ -n "$old_repo_sha" ]; then
+        echo "${repo}: Updating SHA in file"
+        sed -i "s/${old_repo_sha}/${repo},${sha}/" sha.txt
+    else
+        echo "${repo}: SHA not found, writing latest SHA to file"
+        echo "${repo},${sha}" >> sha.txt
+    fi
+}
+
 for dataset in $(cat $dataset_list_path); do
     repo=$(echo $dataset | cut -d ',' -f 1)
     sha=\"$(echo $dataset | cut -d ',' -f 2)\"
     echo "Repo info: $repo,$sha"
 
     echo "${repo}: Running the CLI"
-    ./run_cli_single_dataset.sh $repo
+    cli_output=$(./run_cli_single_dataset.sh $repo)
+    cli_exit_code=$?
 
     # Check if the CLI ran successfully
-    if [ $? -eq 0 ]; then
+    if [ $cli_exit_code -eq 0 ]; then
         echo "${repo}: CLI ran successfully!"
-
-        # Look for an existing SHA for the repo in sha.txt
-        old_repo_sha=$(grep "$repo" sha.txt)
-
-        # The following section is similar to code in sha_scraper.sh but updates SHAs in sha.txt for a different set of repos
-        # Essentially, for a repo that has *successfully run the CLI*, make sure we now record their most up to date SHA
-        # (i.e., to indicate when the last successful CLI run happened).
-        # To achieve this:
-        # - if the repo DOES already have an entry in sha.txt, we replace (update) the existing SHA with the current SHA
-        # - if the repo DOES NOT already have an entry in sha.txt (meaning this is a newly added repo), add the repo and its SHA
-        if [ ! -z "$old_repo_sha" ]; then
-            echo "${repo}: Updating SHA in file"
-            sed -i "s/${old_repo_sha}/${repo},${sha}/" sha.txt
-        else
-            echo "${repo}: SHA not found, writing latest SHA to file"
-            echo $repo,$sha >> sha.txt
-        fi
-    # If the CLI failed for the repo, we do not update or add an entry to sha.txt so that
-    # the next time this script is called, the CLI will be attempted again for that repo.
+        update_sha_file "$repo" "$sha"
     else
+        # Look in the error message for substring from 
+        # "...must contain at least one column with Neurobagel annotations"
+        # to indicate that data dictionary is missing Neurobagel annotations
+        # (we use a substring to avoid issues with logs being wrapped)
+        if echo "$cli_output" | grep -q "one column with Neurobagel annotations"; then
+            echo "${repo}: participants.json is missing Neurobagel annotations. Saving repo ID to datasets_missing_annotations.txt"
+            echo "$repo" >> datasets_missing_annotations.txt
+            update_sha_file "$repo" "$sha"
+        fi
+        # If the CLI failed for the repo and the dataset was annotated, 
+        # we do not update or add an entry to sha.txt so that the next time this script is called, 
+        # the CLI will be attempted again for that repo.
         echo "${repo}: CLI failed"
     fi
 done
--- code/sha_scraper.sh
+++ code/sha_scraper.sh
@@ -22,24 +22,26 @@ reposON_LD=$(gh repo list "$OWNER" --fork --limit ${nRepos} --json name --jq '.[
 
 do_cli_pheno_files_exist() {
     local repo=$1
+    local participant_tsv_response participant_json_response
+
     # Check if participants.tsv exists
-    local participant_tsv_response=$(curl -s -o /dev/null -w "%{http_code}" \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${GH_TOKEN}" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/${OWNER}/${repo}/contents/participants.tsv)
+    participant_tsv_response=$(curl -s -o /dev/null -w "%{http_code}" \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${GH_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/${OWNER}/${repo}/contents/participants.tsv)
 
     # check if participants.json exists
-    local participant_json_response=$(curl -s -o /dev/null -w "%{http_code}" \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${GH_TOKEN}" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/${OWNER}/${repo}/contents/participants.json)
+    participant_json_response=$(curl -s -o /dev/null -w "%{http_code}" \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${GH_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/${OWNER}/${repo}/contents/participants.json)
 
     if (( ($participant_tsv_response >= 200 && $participant_tsv_response < 300 ) && ($participant_json_response >= 200 && $participant_json_response < 300 ) )); then
-        echo true
+        return 0
     else
-        echo false
+        return 1
     fi
 }
 
@@ -48,7 +50,7 @@ touch changed_repos.txt
 for repo in $reposON_LD; do
     # Get the SHA of the latest commit in the repo (NOTE: This will always be from the default branch)
     # TODO: Test if the SHA we grab here changes with a new commit
-    sha=$(curl -L \
+    sha=$(curl -sS -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GH_TOKEN}" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
@@ -58,16 +60,16 @@ for repo in $reposON_LD; do
     line=$(grep "$repo" sha.txt)
 
     # If line with repo SHA is found
-    if [ ! -z "$line" ]; then
+    if [ -n "$line" ]; then
         echo "${repo}: SHA found in file"
         old_sha=$(echo $line | cut -d, -f2)
         # if the SHA is not the same as the old one
-        if [ $old_sha != "$sha" ]; then
+        if [ "$old_sha" != "$sha" ]; then
             echo "${repo}: latest SHA is different than existing SHA"
-            if do_cli_pheno_files_exist $repo == true; then
+            if do_cli_pheno_files_exist "$repo"; then
                 # Add repo ID and current SHA of repo to a list of datasets to run the CLI on
                 echo "${repo}: Adding to job list for CLI"
-                echo $repo,$sha >> changed_repos.txt
+                echo "${repo},${sha}" >> changed_repos.txt
             else
                 # If files needed for CLI not found, simply replace the old SHA with the new one
                 echo "${repo}: participants.json and/or participants.tsv not found"
@@ -80,12 +82,12 @@ for repo in $reposON_LD; do
     else
         echo "${repo}: SHA not found"
         
-        if do_cli_pheno_files_exist $repo == true; then
+        if do_cli_pheno_files_exist "$repo"; then
             echo "${repo}: Adding to job list for CLI"
-            echo $repo,$sha >> changed_repos.txt
+            echo "${repo},${sha}" >> changed_repos.txt
         fi
 
         echo "${repo}: Writing latest SHA to file"
-        echo $repo,$sha >> sha.txt
+        echo "${repo},${sha}" >> sha.txt
     fi
 done
