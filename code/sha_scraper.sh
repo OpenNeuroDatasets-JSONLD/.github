95d9457b1b2033be129efed20e27f445bb967b81
Alyssa Dai <alyssa.ydai@gmail.com>
2025-09-19 01:01:35 -0400

    add flag to sha_scraper.sh to determine whether to consider all repos - also prevent writing to sha.txt for new repos that have CLI input files

--- code/sha_scraper.sh
+++ code/sha_scraper.sh
@@ -6,6 +6,8 @@
 # If both files exist, the repo ID and current SHA are added to a list of datasets to run the CLI on.
 # If a file needed for the CLI is not found, the script only updates the SHA in sha.txt without doing anything else.
 
+flag="$1"
+
 OWNER="OpenNeuroDatasets-JSONLD"
 
 # TODO: Refactor out code to get list of repos in the organization, since we reuse this in run_cli_on_all_repos.yml
@@ -45,8 +47,8 @@ do_cli_pheno_files_exist() {
     fi
 }
 
-# Make empty file to keep track of repos that have changed
-touch changed_repos.txt
+# Make empty file to keep track of repos to run the CLI on
+touch repos_for_cli.txt
 for repo in $reposON_LD; do
     # Get the SHA of the latest commit in the repo (NOTE: This will always be from the default branch)
     # TODO: Test if the SHA we grab here changes with a new commit
@@ -56,38 +58,48 @@ for repo in $reposON_LD; do
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/${OWNER}/${repo}/commits | jq .[0].sha)
 
-    # Get the line with the old SHA from sha.txt for the repo
-    line=$(grep "$repo" sha.txt)
-
-    # If line with repo SHA is found
-    if [ -n "$line" ]; then
-        echo "${repo}: SHA found in file"
-        old_sha=$(echo $line | cut -d, -f2)
-        # if the SHA is not the same as the old one
-        if [ "$old_sha" != "$sha" ]; then
-            echo "${repo}: latest SHA is different than existing SHA"
-            if do_cli_pheno_files_exist "$repo"; then
-                # Add repo ID and current SHA of repo to a list of datasets to run the CLI on
-                echo "${repo}: Adding to job list for CLI"
-                echo "${repo},${sha}" >> changed_repos.txt
-            else
-                # If files needed for CLI not found, simply replace the old SHA with the new one
-                echo "${repo}: participants.json and/or participants.tsv not found"
-                echo "${repo}: Updating SHA in file"
-                sed -i "s/${line}/${repo},${sha}/" sha.txt
-            fi
-        fi   
-    # If repo not found in file, write current SHA to file 
-    # and add repo to list of datasets to run the CLI on, if the files needed for the CLI are found
-    else
-        echo "${repo}: SHA not found"
-        
+    if [[ "$flag" == "--all-repos" ]]; then
         if do_cli_pheno_files_exist "$repo"; then
             echo "${repo}: Adding to job list for CLI"
-            echo "${repo},${sha}" >> changed_repos.txt
+            echo "${repo},${sha}" >> repos_for_cli.txt
+        else
+            echo "${repo}: Writing latest SHA to file"
+            echo "${repo},${sha}" >> sha.txt
         fi
+    else
+        # Get the line with the old SHA from sha.txt for the repo
+        line=$(grep "$repo" sha.txt)
 
-        echo "${repo}: Writing latest SHA to file"
-        echo "${repo},${sha}" >> sha.txt
+        # If line with repo SHA is found
+        if [ -n "$line" ]; then
+            echo "${repo}: SHA found in file"
+            old_sha=$(echo $line | cut -d, -f2)
+            # if the SHA is not the same as the old one
+            if [ "$old_sha" != "$sha" ]; then
+                echo "${repo}: latest SHA is different than existing SHA"
+                if do_cli_pheno_files_exist "$repo"; then
+                    # Add repo ID and current SHA of repo to a list of datasets to run the CLI on
+                    echo "${repo}: Adding to job list for CLI"
+                    echo "${repo},${sha}" >> repos_for_cli.txt
+                else
+                    # If files needed for CLI not found, simply replace the old SHA with the new one
+                    echo "${repo}: participants.json and/or participants.tsv not found"
+                    echo "${repo}: Updating SHA in file"
+                    sed -i "s/${line}/${repo},${sha}/" sha.txt
+                fi
+            fi   
+        # If repo not found in sha.txt, add repo to list of datasets to run the CLI on if the required input files are found,
+        # otherwise write current SHA to file 
+        else
+            echo "${repo}: SHA not found"
+            if do_cli_pheno_files_exist "$repo"; then
+                echo "${repo}: Adding to job list for CLI"
+                echo "${repo},${sha}" >> repos_for_cli.txt
+            else
+                echo "${repo}: participants.json and/or participants.tsv not found"
+                echo "${repo}: Writing latest SHA to file"
+                echo "${repo},${sha}" >> sha.txt
+            fi
+        fi
     fi
 done
