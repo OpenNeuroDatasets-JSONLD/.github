b69966cb42c8891c1797b29b1da9360f76308ea0
Sebastian Urchs <surchs@users.noreply.github.com>
2026-01-08 18:53:34 -0500

    Add little helper script to make dataset_description (#57)
    * Add little helper script to make dataset_description
    
    * Apply suggestions from code review
    
    * Add other suggestion
    
    * More review suggestions
    
    * Remove section on dataset name

--- code/run_cli_single_dataset.sh
+++ code/run_cli_single_dataset.sh
@@ -6,7 +6,10 @@ set -euo pipefail
 ds_id=$1
 
 # Get the data
-ds_portal="https://github.com/OpenNeuroDatasets-JSONLD/${ds_id}.git"
+ds_repository="https://github.com/OpenNeuroDatasets-JSONLD/${ds_id}.git"
+ds_homepage="https://openneuro.org/datasets/${ds_id}"
+
+script_dir="$(dirname "$0")"
 
 ldin=data
 ldout=${ldin}/jsonld
@@ -21,34 +24,20 @@ bids_jsonld_path="${ldout}/${ds_id}_bids.jsonld"
 np_status="${workdir}/../../../openneuro-annotations/processing_status_files/${ds_id}.tsv"
 derivative_jsonld_path="${ldout}/${ds_id}_derivative.jsonld"
 
-datalad clone ${ds_portal} ${workdir}
+datalad clone ${ds_repository} ${workdir}
 datalad get -d $workdir "${workdir}/participants.tsv"
 datalad get -d $workdir "${workdir}/participants.json"
 datalad get -d $workdir "${workdir}/dataset_description.json"
 
-# Get the dataset label
-ds_name=$(cat ${workdir}/dataset_description.json 2>/dev/null | jq .Name)
-
-# Strip the leading and trailing quotes (") from the ds_name, which are preserved by default when using jq
-ds_name=${ds_name#\"}
-ds_name=${ds_name%\"}
-
-# Catches the cases where:
-# - the dataset_description.json does not exist
-# - the "Name" field does not exist
-# - the "Name" field is an empty string "" or contains only whitespace characters (spaces, tabs, newlines, such as " ")
-# and sets the dataset name to the dataset ID in those cases
-if [ -z "$ds_name" ] || [ "$ds_name" == "null" ] || [[ "$ds_name" =~ ^[[:space:]]*$ ]]; then
-    ds_name=$ds_id
-fi
+# Update the description
+python ${script_dir}/update_dataset_description.py ${workdir}/dataset_description.json ${ds_id} ${ds_homepage} ${ds_repository}
 
 # Run the Neurobagel CLI
 bagel pheno \
     --pheno ${workdir}/participants.tsv \
     --dictionary ${workdir}/participants.json \
     --output ${workdir}/pheno.jsonld \
-    --name "$ds_name" \
-    --portal $ds_portal
+    --dataset_description ${workdir}/nb_dataset_description.json \
 
 bagel bids2tsv --bids-dir ${workdir} --output ${workdir}/${ds_id}_bids.tsv
 
---
+++ code/update_dataset_description.py
@@ -0,0 +1,75 @@
+"""
+Create or update a dataset_description.json with relevant metadata for the CLI.
+See: https://neurobagel.org/user_guide/dataset_description/ for reference
+"""
+
+import argparse
+import json
+import logging
+from json import JSONDecodeError
+from pathlib import Path
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def main(ds_description_path: Path, ds_id: str, ds_website: str, ds_repos: str):
+    """
+    Update an existing dataset_description.json file with relevant metadata for the CLI.
+    """
+    try:
+        with open(ds_description_path, encoding="utf-8") as f:
+            ds_description = json.load(f)
+    except (JSONDecodeError, FileNotFoundError) as e:
+        logger.error(f"Could not read dataset_description.json at {ds_description_path}: {e}")
+        ds_description = {}
+
+    # Figure out if ds has a name and if not, set it to ds_id
+    ds_name = ds_description.get("Name", "")
+    if ds_name.strip() == "":
+        ds_name = ds_id
+
+    expanded_ds_description = {
+        **ds_description,
+        "Name": ds_name,
+        "RepositoryURL": ds_repos,
+    }
+
+    if not ds_website in ds_description.get("ReferencesAndLinks", []):
+        expanded_ds_description = {
+            **expanded_ds_description,
+            "ReferencesAndLinks": [
+                ds_website,
+                *ds_description.get("ReferencesAndLinks", []),
+            ]
+        }
+
+    # Write out new json
+    out_path = ds_description_path.parent / "nb_dataset_description.json"
+    with open(out_path, "w", encoding="utf-8") as out_file:
+        json.dump(expanded_ds_description, out_file, indent=4)
+
+    logger.info(f"Wrote updated dataset_description.json to {out_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create or update a dataset_description.json with relevant metadata for the CLI."
+    )
+    parser.add_argument(
+        "ds_description_path",
+        type=Path,
+        help="Path to the dataset_description.json file.",
+    )
+    parser.add_argument(
+        "ds_id", type=str, help="The dataset id (e.g. ds1234)."
+    )
+    parser.add_argument(
+        "ds_homepage", type=str, help="The OpenNeuro website for the dataset."
+    )
+    parser.add_argument(
+        "ds_repo", type=str, help="The GH or datalad repo for the dataset."
+    )
+    args = parser.parse_args()
+    main(args.ds_description_path, args.ds_id, args.ds_homepage, args.ds_repo)
