1e09e5c98a19cc962e2d31d39f08796bcede1116
Sebastian Urchs <sebastian.urchs@gmail.com>
2026-01-08 18:33:00 -0500

    Apply suggestions from code review

--- code/update_dataset_description.py
+++ code/update_dataset_description.py
@@ -6,6 +6,7 @@ See: https://neurobagel.org/user_guide/dataset_description/ for reference
 import argparse
 import json
 import logging
+from json import JSONDecodeError
 from pathlib import Path
 
 
@@ -17,22 +18,27 @@ def main(ds_description_path: Path, ds_id: int, ds_website: str, ds_repos: str):
     """
     Update an existing dataset_description.json file with relevant metadata for the CLI.
     """
-    ds_description = json.load(open(ds_description_path))
+    try:
+        with open(ds_description_path, encoding="utf-8") as f:
+            ds_description = json.load(f)
+    except (JSONDecodeError, FileNotFoundError) as e:
+        logger.error(f"Could not read dataset_description.json at {ds_description_path}: {e}")
+        ds_description = {}
 
     # Figure out if ds has a name and if not, set it to ds_id
     ds_name = ds_description.get("Name", "")
     if ds_name.strip() == "":
-        ds_name = str(ds_id)
+        ds_name = ds_id
 
-    addition = {
+    expanded_ds_description = {
         **ds_description,
         "Name": ds_name,
         "RepositoryURL": ds_repos,
     }
 
     if not ds_website in ds_description.get("ReferencesAndLinks", []):
-        addition = {
-            **addition,
+        expanded_ds_description = {
+            **expanded_ds_description,
             "ReferencesAndLinks": [
                 ds_website,
                 *ds_description.get("ReferencesAndLinks", []),
@@ -41,8 +47,8 @@ def main(ds_description_path: Path, ds_id: int, ds_website: str, ds_repos: str):
 
     # Write out new json
     out_path = ds_description_path.parent / "nb_dataset_description.json"
-    with open(out_path, "w") as out_file:
-        json.dump(addition, out_file, indent=4)
+    with open(out_path, "w", encoding="utf-8") as out_file:
+        json.dump(expanded_ds_description, out_file, indent=4)
 
     logger.info(f"Wrote updated dataset_description.json to {out_path}")
 
@@ -57,13 +63,13 @@ if __name__ == "__main__":
         help="Path to the dataset_description.json file.",
     )
     parser.add_argument(
-        "ds_id", type=int, help="The dataset id."
+        "ds_id", type=str, help="The dataset id (e.g. ds1234)."
     )
     parser.add_argument(
-        "ds_website", type=str, help="The OpenNeuro website for the dataset."
+        "ds_homepage", type=str, help="The OpenNeuro website for the dataset."
     )
     parser.add_argument(
-        "ds_repos", type=str, help="The GH or datalad repo for the dataset."
+        "ds_repo", type=str, help="The GH or datalad repo for the dataset."
     )
     args = parser.parse_args()
-    main(args.ds_description_path, args.ds_id, args.ds_website, args.ds_repos)
+    main(args.ds_description_path, args.ds_id, args.ds_homepage, args.ds_repo)
